4e143c740526ac6bf994f1344d42c199f84d3d65
Anton <137686641+chikibambony137@users.noreply.github.com>
2025-11-04 02:23:21 +0300

    edit error.vue page

---
+++ app/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="eblan">
+    <h1>ты еблан</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+.eblan {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    margin: auto;
+}
+</style>
