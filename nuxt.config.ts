b5ca42d3076a9fa574f37a2897a608e40cd84981
Anton <137686641+chikibambony137@users.noreply.github.com>
2025-11-04 23:39:23 +0300

    add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,25 +2,26 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Ярославский промышленно-экономический колледж', // default fallback title
+      title: "Ярославский промышленно-экономический колледж", // default fallback title
       htmlAttrs: {
-        lang: 'ru',
+        lang: "ru",
       },
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/logo.png' },
-      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/logo.png" }],
 
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
         {
-          name: 'google-site-verification',
-          content: 'KqpNGuL7lFpZpA3YIngmW-DWzOBMRVWVtm8M1qgIVfs',
-        }
-      ]
+          name: "google-site-verification",
+          content: "KqpNGuL7lFpZpA3YIngmW-DWzOBMRVWVtm8M1qgIVfs",
+        },
+      ],
     },
   },
-  modules: ['@pinia/nuxt'],
-  compatibilityDate: '2025-07-15',
+  modules: ["@pinia/nuxt", "@nuxtjs/sitemap"],
+
+  sitemap: {},
+
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: false },
-})
+});
