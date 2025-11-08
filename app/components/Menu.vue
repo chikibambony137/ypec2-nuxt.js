95d9fd8ceb9a6b60386738b621e00e4ba1a54cfc
Anton <137686641+chikibambony137@users.noreply.github.com>
2025-11-08 21:12:39 +0300

    edit menu routing

--- app/components/Menu.vue
+++ app/components/Menu.vue
@@ -1,21 +1,32 @@
 <template>
   <nav class="menu">
-    <a class="menu__ref" href="/"><img src="../assets/news.png" alt="news_img">Новости</a>
-    <a class="menu__ref" href="/abiturient"><img src="../assets/abitur.png" alt="abitur_img">Абитуриентам</a>
-    <a class="menu__ref" href="/teacher"><img src="../assets/prepod.png" alt="prepod_img">Преподавателям</a>
-    <a class="menu__ref" href="/student"><img src="../assets/stud.png" alt="stud_img">Студентам</a>
-    <a class="menu__ref" href="/about"><img src="../assets/about.png" alt="about_img">О колледже</a>
-    <a class="menu__ref" href="/tech"><img src="../assets/tech.png" alt="tech_img">Техническое отделение</a>
-    <a class="menu__ref" href="/himtech"><img src="../assets/himtech.png" alt="himtech_img">Химико-технологическое отделение</a>
-    <a class="menu__ref" href="/finprav"><img src="../assets/finprav.png" alt="finprav_img">Финансово-правовое отделение</a>
-    <a class="menu__ref" href="/distance"><img src="../assets/zaoch.png" alt="zaoch_img">Заочное отделение</a>
-    <a class="menu__ref" href="/dpo"><img src="../assets/dopobr.png" alt="dopobr_img">Дополнительное образование</a>
-    <a class="menu__ref" href="/professionalitet"><img src="../assets/professionalitet.png" alt="prof_img">Профессионалитет</a>
-    <a class="menu__ref" href="/workshops"><img src="../assets/workshops.png" alt="workshops_img">Мастерские</a>
+    <a class="menu__ref" @click="routeTo($event, '/')"><img src="../assets/news.png" alt="news_img">Новости</a>
+    <a class="menu__ref" @click="routeTo($event, '/abiturient')"><img src="../assets/abitur.png" alt="abitur_img">Абитуриентам</a>
+    <a class="menu__ref" @click="routeTo($event, '/teacher')"><img src="../assets/prepod.png" alt="prepod_img">Преподавателям</a>
+    <a class="menu__ref" @click="routeTo($event, '/student')"><img src="../assets/stud.png" alt="stud_img">Студентам</a>
+    <a class="menu__ref" @click="routeTo($event, '/about')"><img src="../assets/about.png" alt="about_img">О колледже</a>
+    <a class="menu__ref" @click="routeTo($event, '/tech')"><img src="../assets/tech.png" alt="tech_img">Техническое отделение</a>
+    <a class="menu__ref" @click="routeTo($event, '/himtech')"><img src="../assets/himtech.png" alt="himtech_img">Химико-технологическое отделение</a>
+    <a class="menu__ref" @click="routeTo($event, '/finprav')"><img src="../assets/finprav.png" alt="finprav_img">Финансово-правовое отделение</a>
+    <a class="menu__ref" @click="routeTo($event, '/distance')"><img src="../assets/zaoch.png" alt="zaoch_img">Заочное отделение</a>
+    <a class="menu__ref" @click="routeTo($event, '/dpo')"><img src="../assets/dopobr.png" alt="dopobr_img">Дополнительное образование</a>
+    <a class="menu__ref" @click="routeTo($event, '/professionalitet')"><img src="../assets/professionalitet.png" alt="prof_img">Профессионалитет</a>
+    <a class="menu__ref" @click="routeTo($event, '/workshops')"><img src="../assets/workshops.png" alt="workshops_img">Мастерские</a>
   </nav>
 </template>
 
-<script setup></script>
+<script setup>
+
+const router = useRouter();
+
+const routeTo = (e, to) => {
+  router.push(to);
+  const refs = document.querySelectorAll(".menu__ref");
+  refs.forEach((ref) => ref.classList.remove("menu__ref_highlight"));
+  e.target.classList.add("menu__ref_highlight");
+}
+
+</script>
 
 <style scoped lang="scss">
 @use "../assets/var.scss";
@@ -46,5 +57,9 @@
   &:hover {
     color: rgb(19, 176, 147);
   }
+}
+
+.menu__ref_highlight {
+  color: rgb(19, 176, 147);
 }
 </style>
